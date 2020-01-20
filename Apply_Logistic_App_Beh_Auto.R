1f1f330d863d0f98c5cacad6298a5d07c32ed65e
Radi Sadek <rsadek@cc.local>
2020-01-20 14:49:28 +0200

    Apply auto upload

--- Apply_Logistic_App_Beh_Auto.R
+++ Apply_Logistic_App_Beh_Auto.R
@@ -12,59 +12,100 @@
 ### Initial settings ###
 ########################
 
-# Library
+# Libraries
 suppressMessages(suppressWarnings(library(RMySQL)))
 suppressMessages(suppressWarnings(library(here)))
 suppressMessages(suppressWarnings(library(dotenv)))
 suppressMessages(suppressWarnings(require("reshape")))
-suppressMessages(suppressWarnings(library(openxlsx)))
 
 
-# Database
-db_user <- "root"
-db_password <- "123456"
-db_name <- "citycash_db"
-db_host <- "127.0.0.1"
-df_port <- 3306
-con <- dbConnect(MySQL(), user=db_user, password=db_password, 
-                 dbname=db_name, host=db_host, port = df_port)
+# Defines the directory where custom .env file is located
+load_dot_env(file = here('.env'))
 
 
-# Define work directory
-main_dir <- "C:\\Projects\\Apply_Scoring\\"
 
+#########################
+### Command arguments ###
+#########################
 
-# Read argument of ID
 args <- commandArgs(trailingOnly = TRUE)
-#application_id <- args[1]
-application_id <- 523820
-product_id <- NA
+application_id <- args[1]
+product_id <- args[2]
 
 
-# Set working directory for input (R data for logistic regression) and output #
-setwd(main_dir)
+#######################
+### Manual settings ###
+#######################
 
+# Defines the directory where the RScript is located
+base_dir <- here('app/Factories/Scoring')
+
+
+
+#####################
+####### MySQL #######
+#####################
+
+db_host <- Sys.getenv("DB_HOST", 
+                      unset = "localhost", 
+                      names = FALSE)
+db_port <- strtoi(Sys.getenv("DB_PORT", 
+                             unset = "3306", 
+                             names = FALSE))
+db_name <- Sys.getenv("DB_DATABASE", 
+                      unset = "citycash", 
+                      names = FALSE)
+db_username <- Sys.getenv("DB_USERNAME", 
+                          unset = "root", 
+                          names = FALSE)
+db_password <- Sys.getenv("DB_PASSWORD", 
+                          unset = "secret", 
+                          names = FALSE)
+con <- dbConnect(MySQL(), user=db_username, 
+                 password=db_password, dbname=db_name, 
+                 host=db_host, port = db_port)
+sqlMode <- paste("SET sql_mode=''", sep ="")
+suppressWarnings(fetch(dbSendQuery(con, sqlMode), 
+                       n=-1))
+
+
+#################################
+####### Load source files #######
+#################################
 
 # Load other r files
-source(paste(main_dir,"R\\Project\\Logistic_App_CityCash.R", sep=""))
-source(paste(main_dir,"R\\Project\\Logistic_App_Credirect.R"))
-source(paste(main_dir,"R\\Project\\Logistic_Beh_CityCash.R", sep=""))
-source(paste(main_dir,"R\\Project\\Logistic_Beh_Credirect.R", sep=""))
-source(paste(main_dir,"R\\Project\\Useful_Functions.R", sep=""))
-source(paste(main_dir,"R\\Project\\Empty_Fields.R", sep=""))
-source(paste(main_dir,"R\\Project\\Cutoffs.R", sep=""))
-source(paste(main_dir,"R\\Project\\SQL_queries.R", sep=""))
-source(paste(main_dir,"R\\Project\\Disposable_Income.R", sep=""))
-source(paste(main_dir,"R\\Project\\Behavioral_Variables.R", sep=""))
-source(paste(main_dir,"R\\Project\\Normal_Variables.R", sep=""))
-source(paste(main_dir,"R\\Project\\CKR_variables.R", sep=""))
+source(file.path(base_dir,"Logistic_App_CityCash.r"))
+source(file.path(base_dir,"Logistic_App_Credirect.r"))
+source(file.path(base_dir,"Logistic_Beh_CityCash.r"))
+source(file.path(base_dir,"Logistic_Beh_Credirect.r"))
+source(file.path(base_dir,"Useful_Functions.r"))
+source(file.path(base_dir,"Empty_Fields.r"))
+source(file.path(base_dir,"Cutoffs.r"))
+source(file.path(base_dir,"SQL_queries.r"))
+source(file.path(base_dir,"Disposable_Income.r"))
+source(file.path(base_dir,"Behavioral_Variables.r"))
+source(file.path(base_dir,"Normal_Variables.r"))
+source(file.path(base_dir,"CKR_variables.r"))
 
+
+
+########################
+####### Settings #######
+########################
 
 # Load predefined libraries
-load("rdata\\all_repeat.rdata")
-load("rdata\\citycash_app.rdata")
-load("rdata\\credirect_app.rdata")
-load("rdata\\credirect_repeat.rdata")
+rdata <- file.path(base_dir, "rdata", 
+                   "all_repeat.rdata")
+load(rdata)
+rdata2 <- file.path(base_dir, "rdata", 
+                    "citycash_app.rdata")
+load(rdata2)
+rdata3 <- file.path(base_dir, "rdata", 
+                    "credirect_app.rdata")
+load(rdata3)
+rdata4 <- file.path(base_dir, "rdata", 
+                    "credirect_repeat.rdata")
+load(rdata4)
 
 
 ####################################
@@ -73,9 +114,9 @@ load("rdata\\credirect_repeat.rdata")
 
 # Read credits applications
 all_df <- suppressWarnings(fetch(dbSendQuery(con, 
-              gen_big_sql_query(db_name,application_id)), n=-1))
+            gen_big_sql_query(db_name,application_id)), n=-1))
 all_df$date <- ifelse(all_df$status %in% c(4,5), all_df$signed_at, 
-                      all_df$created_at)
+           all_df$created_at)
 
 
 # Apply some checks to main credit dataframe
@@ -89,26 +130,26 @@ if(nrow(all_df)>1){
 
 # Read product's periods and amounts
 products  <- suppressWarnings(fetch(dbSendQuery(con, 
-               gen_products_query(db_name,all_df)), n=-1))
+         gen_products_query(db_name,all_df)), n=-1))
 products_desc <- suppressWarnings(fetch(dbSendQuery(con, 
-               gen_products_query_desc(db_name,all_df)), n=-1))
+         gen_products_query_desc(db_name,all_df)), n=-1))
 
 
 # Read all previous credits or applications of client
 all_credits <- suppressWarnings(fetch(dbSendQuery(con, 
-      gen_all_credits_query(db_name,all_df)), n=-1))
+         gen_all_credits_query(db_name,all_df)), n=-1))
 all_credits$date <- ifelse(all_credits$status %in% c(4,5), 
-      all_credits$signed_at, all_credits$created_at)
+        all_credits$signed_at, all_credits$created_at)
 
 
 # Check if client has a risk profile
 risk <- suppressWarnings(fetch(dbSendQuery(con, 
-    gen_risky_query(db_name,all_df)), n=-1))
+        gen_risky_query(db_name,all_df)), n=-1))
 
 
 # Read total amount of current credit
 total_amount_curr <- suppressWarnings(fetch(dbSendQuery(con, 
-    gen_total_amount_curr_query(db_name,application_id)), n=-1))
+        gen_total_amount_curr_query(db_name,application_id)), n=-1))
 
 
 # Read CKR 
@@ -118,19 +159,19 @@ data_ckr_financial <- gen_query_ckr(2)
 
 # Read all previous active or terminated credits of client
 all_id <- subset(all_credits, all_credits$id==application_id | 
-    (all_credits$status %in% c(4,5) &
-    (all_credits$sub_status!=129 | is.na(all_credits$sub_status)) & 
-     all_credits$client_id==all_df$client_id))
+            (all_credits$status %in% c(4,5) &
+            (all_credits$sub_status!=129 | is.na(all_credits$sub_status)) & 
+             all_credits$client_id==all_df$client_id))
 all_id <- subset(all_id, all_id$date<=all_id$date[all_id$id==application_id])
 
 
 # Select for max delay if past AND active: must have at least 30 days of passed
 all_id_max_delay <- all_id[all_id$id != application_id,]
 all_actives_past <- subset(all_id, 
-    all_id$id!=application_id & all_id$status==4)
+                           all_id$id!=application_id & all_id$status==4)
 if(nrow(all_actives_past)>0){
   all_id_max_delay <- gen_select_relevant_ids_max_delay(db_name,all_actives,
-      all_id_max_delay)
+         all_id_max_delay)
 }
 
 
@@ -147,20 +188,20 @@ if (nrow_all_id>1){
 nrow_all_id_max_delay <- nrow(all_id_max_delay)
 if (nrow_all_id_max_delay>=1){
   list_ids_max_delay <- gen_select_relevant_ids(all_id_max_delay,
-     nrow_all_id_max_delay)
+      nrow_all_id_max_delay)
   data_plan_main_select <- suppressWarnings(fetch(dbSendQuery(con, 
-     gen_plan_main_select_query(db_name,list_ids_max_delay)), n=-1))
+      gen_plan_main_select_query(db_name,list_ids_max_delay)), n=-1))
 } 
 
 
 # Get average expenses according to client's address 
 addresses <- suppressWarnings(fetch(dbSendQuery(con, 
-  gen_address_query(all_df$client_id,"App\\\\Models\\\\Clients\\\\Client")), 
-  n=-1))
+       gen_address_query(all_df$client_id,"App\\\\Models\\\\Clients\\\\Client")), 
+       n=-1))
 if(nrow(addresses)==0){
   addresses <- suppressWarnings(fetch(dbSendQuery(con, 
-  gen_address_query(all_df$client_id,
-  "App\\\\Models\\\\Credits\\\\Applications\\\\Application")), n=-1))
+       gen_address_query(all_df$client_id,
+       "App\\\\Models\\\\Credits\\\\Applications\\\\Application")), n=-1))
 }
 
 
@@ -170,7 +211,7 @@ if(nrow(addresses)==0){
 
 # Compute flag if credit is up to next salary
 flag_credit_next_salary <- ifelse(all_df$product_id %in% 
-      c(25:28,36,37,41:44,49,50,55:58), 1, 0)
+                                    c(25:28,36,37,41:44,49,50,55:58), 1, 0)
 
 
 # Compute flag if product is credirect
@@ -204,7 +245,7 @@ all_df <- gen_norm_var(period,all_df,products)
 
 # Compute and generate variables specific for behavioral model
 data_plan_main_select_def <- ifelse(exists("data_plan_main_select"),
-          data_plan_main_select,NA)
+                                    data_plan_main_select,NA)
 all_df <- gen_other_rep(nrow_all_id,all_id,all_df,flag_credirect,
                         data_plan_main_select_def)
 
@@ -225,7 +266,7 @@ all_df <- gen_prev_online(db_name, all_credits,all_df)
 # Compute flag if last paid credit is maybe hidden refinance
 all_df <- gen_ratio_last_amount_paid(db_name,all_credits,all_df)
 
-                                
+
 # Compute amount differential 
 all_df$amount_diff <- ifelse(nrow_all_id<=1, NA, all_df$amount - 
                                prev_amount$amount)
@@ -234,7 +275,7 @@ all_df$amount_diff <- ifelse(nrow_all_id<=1, NA, all_df$amount -
 # Compute income variables
 t_income <- gen_t_income(db_name,application_id,period)
 disposable_income_adj <- gen_disposable_income_adj(db_name,application_id,
-        all_df,addresses,period)
+                                                   all_df,addresses,period)
 all_df$total_income <- gen_income(db_name,application_id)
 
 
@@ -257,14 +298,14 @@ df <- gen_null_to_na(df)
 # Get if empty field threshold is passed
 empty_fields <- gen_empty_fields(flag_beh,flag_credirect,df)
 threshold_empty <- ifelse(flag_credirect==0 & flag_beh==0, 7,
-   ifelse(flag_credirect==1 & flag_beh==0, 4,
-   ifelse(flag_credirect==0 & flag_beh==1, 8, 5)))
+                   ifelse(flag_credirect==1 & flag_beh==0, 4,
+                   ifelse(flag_credirect==0 & flag_beh==1, 8, 5)))
 
 
 # Adjust count of empty fields accordingly
 empty_fields <- ifelse(flag_credirect==1, empty_fields, 
-   ifelse(is.na(df$total_income) | df$total_income==0, 
-   threshold_empty, empty_fields))
+                ifelse(is.na(df$total_income) | df$total_income==0, 
+                       threshold_empty, empty_fields))
 
 
 # Readjust fields
@@ -291,20 +332,20 @@ if (empty_fields>=threshold_empty){
   
 } else if (flag_beh==1 & flag_credirect==0){
   scoring_df <- gen_beh_citycash(df,scoring_df,products,df_Log_beh,period,
-                     all_df,prev_amount,amount_tab,
-                     t_income,disposable_income_adj)
+                                 all_df,prev_amount,amount_tab,
+                                 t_income,disposable_income_adj)
 } else if (flag_beh==1 & flag_credirect==1){
   scoring_df <- gen_beh_credirect(df,scoring_df,products,df_Log_beh,period,
-                     all_df,prev_amount,amount_tab,
-                     t_income,disposable_income_adj)
+                                  all_df,prev_amount,amount_tab,
+                                  t_income,disposable_income_adj)
 } else if (flag_beh==0 & flag_credirect==0){
   scoring_df <- gen_app_citycash(df,scoring_df,products,df_Log_beh,period,
-                     all_df,prev_amount,amount_tab,
-                     t_income,disposable_income_adj)
+                                 all_df,prev_amount,amount_tab,
+                                 t_income,disposable_income_adj)
 } else {
   scoring_df <- gen_app_credirect(df,scoring_df,products,df_Log_beh,period,
-                     all_df,prev_amount,amount_tab,
-                     t_income,disposable_income_adj)
+                                  all_df,prev_amount,amount_tab,
+                                  t_income,disposable_income_adj)
 }
 
 
@@ -330,10 +371,10 @@ if(flag_cession==1 & flag_credirect==1){
 
 # Create output dataframe
 final <- as.data.frame(cbind(scoring_df$application_id[1],
-     scoring_df$score[scoring_df$amount== unique(scoring_df$amount)
+          scoring_df$score[scoring_df$amount== unique(scoring_df$amount)
           [which.min(abs(all_df$amount - unique(scoring_df$amount)))]
-                      & 
-     scoring_df$period==unique(scoring_df$period)
+                  & 
+          scoring_df$period==unique(scoring_df$period)
           [which.min(abs(all_df$installments - unique(scoring_df$period)))]]))
 names(final) <- c("id","score")
 final$flag_beh <- flag_beh
