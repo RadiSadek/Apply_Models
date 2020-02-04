36814b23de35b64aecab532be49bf2c2b28cac48
Radi Sadek <rsadek@cc.local>
2020-02-04 15:07:08 +0200

    decreasing cutoffs for application Credirect

--- Cutoffs.R
+++ Cutoffs.R
@@ -8,13 +8,17 @@
 cu_app_city <- c(0.375,0.3,0.225,0.15,0.075)
 
 # Application Credirect Flex
-cu_app_cred_flex <- c(0.3,0.175,0.125,0.075,0.05)
+cu_app_cred_flex <- c(0.25,0.175,0.125,0.075,0.05)
 
 # Application Credirect Potrebitelski
-cu_app_cred_user <- c(0.3,0.225,0.175,0.125,0.075)
+cu_app_cred_user <- c(0.25,0.2,0.175,0.125,0.075)
 
 # Repeat City Cash
 cu_beh_city <- c(0.3,0.25,0.2,0.15,0.1)
 
 # Repeat Credirect
 cu_beh_cred <- c(0.35,0.275,0.2,0.125,0.075)
+
+# Old application Credirect
+#cu_app_cred_flex <- c(0.3,0.175,0.125,0.075,0.05)
+#cu_app_cred_user <- c(0.3,0.225,0.175,0.125,0.075)
