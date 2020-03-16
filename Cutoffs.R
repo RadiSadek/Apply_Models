de8808ce1fb36ad614f4fa6abfcb002c38374157
Radi Sadek <rsadek@cc.local>
2020-03-16 09:25:34 +0200

    update coronavirus cutoffs

--- Cutoffs.R
+++ Cutoffs.R
@@ -4,21 +4,47 @@
 ######## Define cutoffs for statistical models  ######
 ######################################################
 
+
+#### Cutoffs after Coronvirus
+
 # # Application City Cash
-cu_app_city_bad_offices <- c(0.325,0.25,0.2,0.15,0.075)
-cu_app_city_norm_offices <- c(0.375,0.3,0.225,0.15,0.075)
+cu_app_city_bad_offices <- c(0.3,0.25,0.2,0.15,0.075)
+cu_app_city_norm_offices <- c(0.35,0.3,0.225,0.15,0.075)
 
 # Application Credirect Flex
-cu_app_cred_flex <- c(0.25,0.175,0.125,0.075,0.05)
+cu_app_cred_flex <- c(0.2125,0.175,0.125,0.075,0.05)
 
 # Application Credirect Potrebitelski
-cu_app_cred_user <- c(0.25,0.2,0.175,0.125,0.075)
+cu_app_cred_user <- c(0.225,0.2,0.175,0.125,0.075)
 
 # Repeat City Cash
-cu_beh_city <- c(0.3,0.25,0.2,0.15,0.1)
+cu_beh_city <- c(0.275,0.25,0.2,0.15,0.1)
 
 # Repeat Credirect
-cu_beh_cred <- c(0.35,0.275,0.2,0.125,0.075)
+cu_beh_cred <- c(0.32,0.275,0.2,0.125,0.075)
+
+
+
+#### Cutoffs before Coronavirus 
+
+# # # Application City Cash
+# cu_app_city_bad_offices <- c(0.325,0.25,0.2,0.15,0.075)
+# cu_app_city_norm_offices <- c(0.375,0.3,0.225,0.15,0.075)
+# 
+# # Application Credirect Flex
+# cu_app_cred_flex <- c(0.25,0.175,0.125,0.075,0.05)
+# 
+# # Application Credirect Potrebitelski
+# cu_app_cred_user <- c(0.25,0.2,0.175,0.125,0.075)
+# 
+# # Repeat City Cash
+# cu_beh_city <- c(0.3,0.25,0.2,0.15,0.1)
+
+# # Repeat Credirect
+# cu_beh_cred <- c(0.35,0.275,0.2,0.125,0.075)
+
+
+#### Oldest cutoffs
 
 # Old application Credirect
 #cu_app_cred_flex <- c(0.3,0.175,0.125,0.075,0.05)
