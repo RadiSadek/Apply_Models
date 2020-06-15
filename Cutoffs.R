f8f0ad26f7fdf8daa3c5dff88828aa994b5cb6ac
Radi Sadek <rsadek@cc.local>
2020-06-15 11:19:17 +0300

    correct cutoffs app credirect installments

--- Cutoffs.R
+++ Cutoffs.R
@@ -15,7 +15,7 @@ cu_app_city_norm_offices <- c(0.35,0.3,0.225,0.15,0.075)
 cu_app_cred_flex <- c(0.125,0.1,0.08,0.05,0.035)
 
 # Application Credirect Potrebitelski
-cu_app_cred_user <- c(0.6,0.55,0.475,0.375,0.325)
+cu_app_cred_user <- c(0.65,0.6,0.5,0.4,0.325)
 
 # Repeat City Cash
 cu_beh_city <- c(0.275,0.25,0.2,0.15,0.1)
