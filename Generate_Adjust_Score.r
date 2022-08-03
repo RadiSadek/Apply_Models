639e219220136ee37401bc00c245fe14f77812a9
RadiSadek <60088784+RadiSadek@users.noreply.github.com>
2022-08-03 10:45:54 +0300

    stop Ind and amount cession >0 - Credirect

--- Generate_Adjust_Score.r
+++ Generate_Adjust_Score.r
@@ -59,7 +59,7 @@ gen_apply_policy <- function(scoring_df,flag_credirect,flag_cession,
      flag_beh_company,flag_cashpoint,crit){
   
   if(flag_cession==1 & flag_credirect==1){
-    scoring_df <- gen_adjust_score(scoring_df, c("Bad"))
+    scoring_df <- gen_adjust_score(scoring_df, c("Bad","Indeterminate"))
   }
   
   # Apply if City Cash application
