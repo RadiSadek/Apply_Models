99cc90758e1f96af0b1d2bf47e45b60e66979959
RadiSadek <60088784+RadiSadek@users.noreply.github.com>
2023-05-03 12:06:43 +0300

    relaxed cutoffs for rep credirect and new flex credirect

--- Cutoffs.R
+++ Cutoffs.R
@@ -21,13 +21,13 @@ cu_beh_city <- c(0.4,0.3,0.225,0.15,0.075)
 ############# Credirect 
 
 # Application Credirect PayDay
-cu_app_cred_flex <- c(0.26,0.125,0.1,0.07,0.045)
+cu_app_cred_flex <- c(0.3,0.125,0.1,0.07,0.045)
 
 # Application Credirect Installments
 cu_app_cred_user <- c(0.65,0.58,0.5,0.425,0.35)
 
 # Repeat all Credirect
-cu_beh_cred <- c(0.7,0.4,0.275,0.175,0.1)
+cu_beh_cred <- c(0.8,0.4,0.275,0.175,0.1)
 
 # Application Credirect Frauds
 cu_app_cred_frauds <- 0.2
